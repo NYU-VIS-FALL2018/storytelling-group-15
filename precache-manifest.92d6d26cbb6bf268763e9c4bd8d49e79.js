d8be1f0668c70638191daddb9530cc244767ac37
Paresh Patil <pareshpatil@10-18-39-191.dynapool.wireless.nyu.edu>
2018-12-05 15:02:31 -0500

    Updates

---
+++ precache-manifest.92d6d26cbb6bf268763e9c4bd8d49e79.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myapp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dff9ed99b8412eec5183b33d57d9c447",
+    "url": "/myapp/static/media/Stanford_MSA_Database.dff9ed99.csv"
+  },
+  {
+    "revision": "8dd91205e0d72f81ec3b",
+    "url": "/myapp/static/js/runtime~main.8dd91205.js"
+  },
+  {
+    "revision": "ea16d64454ac65db9781",
+    "url": "/myapp/static/js/main.ea16d644.chunk.js"
+  },
+  {
+    "revision": "ed0299b03a18324f62aa",
+    "url": "/myapp/static/js/1.ed0299b0.chunk.js"
+  },
+  {
+    "revision": "ea16d64454ac65db9781",
+    "url": "/myapp/static/css/main.149116cd.chunk.css"
+  },
+  {
+    "revision": "57de139bbef9f21640495fa4f97adce3",
+    "url": "/myapp/index.html"
+  }
+];
