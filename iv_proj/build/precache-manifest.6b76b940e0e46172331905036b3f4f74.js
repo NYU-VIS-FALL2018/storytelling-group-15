a4cd5be4a1ae1f20d4a49637c584e48d3b77f5ab
Praphulla Bhawsar <prtsh32@gmail.com>
2018-12-05 16:14:29 -0500

    Adding build directory

---
+++ iv_proj/build/precache-manifest.6b76b940e0e46172331905036b3f4f74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "7ada291d27290c2c8aac",
+    "url": "/storytelling-group-15/iv_proj/build/index.html/static/css/main.324f6825.chunk.css"
+  },
+  {
+    "revision": "7ada291d27290c2c8aac",
+    "url": "/storytelling-group-15/iv_proj/build/index.html/static/js/main.7ada291d.chunk.js"
+  },
+  {
+    "revision": "86b494fde8fdf04809c7",
+    "url": "/storytelling-group-15/iv_proj/build/index.html/static/js/1.86b494fd.chunk.js"
+  },
+  {
+    "revision": "18420a2f80711870d817",
+    "url": "/storytelling-group-15/iv_proj/build/index.html/static/js/runtime~main.18420a2f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/storytelling-group-15/iv_proj/build/index.html/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dff9ed99b8412eec5183b33d57d9c447",
+    "url": "/storytelling-group-15/iv_proj/build/index.html/static/media/Stanford_MSA_Database.dff9ed99.csv"
+  },
+  {
+    "revision": "64f1b2f1e82a55c780ebcd65212c470d",
+    "url": "/storytelling-group-15/iv_proj/build/index.html/index.html"
+  }
+];
