2e6f46a436c76c9365f2e35d0962617be976e315
Paresh Patil <pareshpatil@10-18-39-191.dynapool.wireless.nyu.edu>
2018-12-05 15:24:04 -0500

    Updates

---
+++ precache-manifest.e6bf8cc4fb009c403b602321a47b1c93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/storytelling-group-15//iv_proj/public/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "dff9ed99b8412eec5183b33d57d9c447",
+    "url": "/storytelling-group-15//iv_proj/public/static/media/Stanford_MSA_Database.dff9ed99.csv"
+  },
+  {
+    "revision": "a95285a535ff6a70c241",
+    "url": "/storytelling-group-15//iv_proj/public/static/js/runtime~main.a95285a5.js"
+  },
+  {
+    "revision": "d767e0a0e9b521d07ef4",
+    "url": "/storytelling-group-15//iv_proj/public/static/js/main.d767e0a0.chunk.js"
+  },
+  {
+    "revision": "ed0299b03a18324f62aa",
+    "url": "/storytelling-group-15//iv_proj/public/static/js/1.ed0299b0.chunk.js"
+  },
+  {
+    "revision": "d767e0a0e9b521d07ef4",
+    "url": "/storytelling-group-15//iv_proj/public/static/css/main.149116cd.chunk.css"
+  },
+  {
+    "revision": "36e331becb8189a2ec979ec8e78c51cb",
+    "url": "/storytelling-group-15//iv_proj/public/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/storytelling-group-15/precache-manifest.0d4adebfca42c2274013132f40b8ef19.js"
+  "/storytelling-group-15//iv_proj/public/precache-manifest.e6bf8cc4fb009c403b602321a47b1c93.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/storytelling-group-15/index.html", {
+workbox.routing.registerNavigationRoute("/storytelling-group-15//iv_proj/public/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
