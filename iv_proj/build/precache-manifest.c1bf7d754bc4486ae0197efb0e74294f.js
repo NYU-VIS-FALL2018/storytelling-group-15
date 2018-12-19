8a50941832e8a5398db7b74a43fe0d86580fa1a3
Praphulla Bhawsar <prtsh32@gmail.com>
2018-12-19 17:04:48 -0500

    Adding Build Folder

---
+++ iv_proj/build/precache-manifest.c1bf7d754bc4486ae0197efb0e74294f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fec7f5372f07ffd5c13e",
+    "url": "/storytelling-group-15/iv_proj/build/static/css/main.b523d73f.chunk.css"
+  },
+  {
+    "revision": "fec7f5372f07ffd5c13e",
+    "url": "/storytelling-group-15/iv_proj/build/static/js/main.fec7f537.chunk.js"
+  },
+  {
+    "revision": "d66cb5ca24466ab62a0e",
+    "url": "/storytelling-group-15/iv_proj/build/static/js/1.d66cb5ca.chunk.js"
+  },
+  {
+    "revision": "c9f27448d61494dda792",
+    "url": "/storytelling-group-15/iv_proj/build/static/js/runtime~main.c9f27448.js"
+  },
+  {
+    "revision": "02b7dfd3b80c2d71feba5d93eba61e93",
+    "url": "/storytelling-group-15/iv_proj/build/index.html"
+  }
+];
